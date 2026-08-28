3cb512a4f1d9026ecc2e6d526f857bbe44a8240f
Tuấn Long <tomkidsstore@gmail.com>
2026-08-28 13:28:08 +0700

    u

---
+++ check_paths.py
@@ -0,0 +1,8 @@
+﻿import json
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+with open('ota_data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+for app in data.get('Mạng Xã Hội', []):
+    print(f"{app['name']}: {app['icon']}")
